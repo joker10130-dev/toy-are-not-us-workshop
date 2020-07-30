4be4df9a3a4c1fefdf898211efacf4b1d0b526c3
Suthinan Musitmani <suthinan.musitmani@allianz.com>
2020-07-30 14:49:51 +0700

    Add product detail

--- frontend/src/app/product-detail/product-detail.component.ts
+++ frontend/src/app/product-detail/product-detail.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.css'],
 })
 export class ProductDetailComponent implements OnInit {
-
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute) {}
+  public state: any;
 
   ngOnInit(): void {
+    this.state = JSON.parse(
+      this.activatedRoute.snapshot.queryParamMap.get('toy')
+    );
   }
-
 }
