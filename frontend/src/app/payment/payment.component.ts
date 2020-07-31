5750684565d41eed98088d88333646e94e523335
Suthinan Musitmani <suthinan.musitmani@allianz.com>
2020-07-31 15:01:57 +0700

    Edit payment

--- frontend/src/app/payment/payment.component.ts
+++ frontend/src/app/payment/payment.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToyCart } from '../models/toy-cart';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
-  styleUrls: ['./payment.component.css']
+  styleUrls: ['./payment.component.css'],
 })
 export class PaymentComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {}
+  public carts: ToyCart[];
+  public allItemQuantities = 0;
+  public totalPrice = 0;
+  public shippingFee = 35;
   ngOnInit(): void {
+    this.carts = JSON.parse(
+      this.activatedRoute.snapshot.queryParamMap.get('order')
+    );
+    for (const item of this.carts) {
+      this.allItemQuantities += Number(item.quantity);
+      this.totalPrice += Number(item.price);
+    }
   }
-
+  placeOrder(): void {
+    this.router.navigate(['thanks-you'], {
+      queryParams: { order: JSON.stringify(this.carts) },
+    });
+  }
 }
