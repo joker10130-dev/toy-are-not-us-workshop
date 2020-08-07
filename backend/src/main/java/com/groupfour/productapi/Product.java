b9d6163273cf68aad0dd3efb4a418bac79a9577e
Kanawat Phuengphadung <kanawatphu@mathregit.group>
2020-08-07 15:18:03 +0700

    Product.java done.

--- backend/src/main/java/com/groupfour/productapi/Product.java
+++ backend/src/main/java/com/groupfour/productapi/Product.java
@@ -1,7 +1,72 @@
 package com.groupfour.productapi;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Product {
+    @Id
+    private  int id;
+    private int price;
+    private int quantity;
+    private String name;
+    private String age;
+    private String gender;
+    private String image;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
