d641c28989a71b9f3ecf43bf587d6ed115d6eb77
Narongrit Rodphroboon <narongrit.rodphroboon@allianz.com>
2020-08-07 14:49:57 +0700

    Add Database

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,1 +1,3 @@
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUES (4, 'Hoppity Ball 26', 'Neutral', '3_to_5', 29.95, true, 'SportsFun')
+INSERT INTO product (id, name, gender, age, price, availability, brand) VALUES (4, 'Hoppity Ball 26', 'Neutral', '3_to_5', 29.95, true, 'SportsFun');
+INSERT INTO product (id, name, gender, age, price, availability, brand) VALUES (1, '43 Piece dinner Set', 'Female', '3_to_5', 12.95, true, 'CoolKidz');
+INSERT INTO product (id, name, gender, age, price, availability, brand) VALUES (30, 'Fisher-Price stroller', 'Female', 'Toddler', 25.99, true, 'CoolKidz');
